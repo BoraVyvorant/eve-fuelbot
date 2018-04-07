7cbd1e1e84931d8d2723b66d5c9240e42a1e8a43
Bora Vyvorant <bora.vyvorant@gmail.com>
2018-04-07 19:58:21 +0100

    Initial functional version

--- README.md
+++ README.md
@@ -1,2 +1,99 @@
-# eve-fuelbot
-Notify Slack when structures need fuel
+# EVE FuelBot
+
+This Ruby application can be used to notify Slack channels of the _fuelling
+state_ of a corporation's Upwell structures. It is stateful, so that it can
+be run often but only report state changes.
+
+A structure's fuelling state is one of the following:
+
+* `unknown` is regarded as the previous state for any structures not
+  already present in the state file.
+
+* `danger` means that the structure will run out of fuel in a week or less.
+
+* `warning` means that the structure will run out of fuel in more than a week,
+  but not more than two weeks.
+
+* `good` means that the structure has enough fuel to last at least two weeks.
+
+These arbitrary thresholds are currently hard-coded in `fuelbot.rb`.
+
+## Configuration
+
+### SSO and ESI Configuration
+
+The application takes its configuration from `config.yaml`. This contains
+various secrets, so I haven't included the one I'm using in the repository.
+Instead, `example-config.yaml` can be used as a template.
+
+You need to have registered a third-party application with CCP
+at <https://developers.eveonline.com>; this will give
+you the `client_id` and `client_secret` values to put in the configuration.
+Your application's registration should include at least the following scopes:
+
+* esi-corporations.read_structures.v1
+
+Once you have a registered application, you need to manually use the EVE SSO
+to log in as a character from the target corporation with the "Station Manager"
+role, and request those same scopes. If you don't know how to do this, CCP
+have [a step-by-step article](https://developers.eveonline.com/blog/article/sso-to-authenticated-calls)
+covering the process.
+
+The resulting refresh token goes into the
+`refresh_token` configuration item.
+
+### Operational Configuration
+
+The `systems` configuration item is a list of solar system names; the
+application will only report on structures anchored in these systems.
+
+The application tracks the previous fuelling state of structures so that it can
+notify the Slack channel only when the state changes. This state is held by
+default in the file `state.yaml`, but if you'd like to put the state file
+somewhere else (for example so that you can mount it into a Docker container)
+then its location is taken from the `statefile` configuration item.
+
+If any of the notifications are for anything other than a `good` state,
+the message will be an `@channel` ping.
+
+### Slack Configuration
+
+If there are some state changes to notify (within the given solar systems) then
+Slack's "incoming webhooks" facility is used to make a nice notification with an
+attachment state change. The details are nested under a `slack` configuration
+item:
+
+* The URL you get from your webhook configuration goes in `webhook_url`
+
+* `channel` can be a public or private channel prefixed by `#`, e.g.,
+  `'#fuel-panics'`. It can also be a user name prefixed by `@`.
+
+## Setup and Execution
+
+Set up as follows:
+
+```bash
+$ bundle install --path vendor
+```
+
+Execute like this:
+
+```bash
+$ bundle exec ruby fuelbot.rb
+```
+
+Over in Slack, you might see something like this:
+
+![Slack notification from FuelBot](fuelbot.png)
+
+## Future Plans
+
+The thresholds between fuelling states should be taken from the configuration
+file.
+
+It's possible for Slack messages to report times in the local timezone of each
+reader, and this seems like a good approach (possibly as well as the EVE time
+currently reported).
+
+For my own deployment, I am going to use Docker and once that is done I will
+add appropriate support here.
---
+++ fuelbot.rb
@@ -0,0 +1,146 @@
+require 'date'
+require 'esi-client-bvv'
+require 'oauth2'
+require 'set'
+require 'slack-notifier'
+require 'yaml'
+require 'yaml/store'
+
+config = YAML.load_file('config.yaml')
+
+#
+# Get an OAuth2 access token for ESI.
+#
+
+client = OAuth2::Client.new(config[:client_id], config[:client_secret],
+                            site: 'https://login.eveonline.com')
+
+# Wrap the refresh token.
+refresh_token = OAuth2::AccessToken.new(client, '',
+                                        refresh_token: config[:refresh_token])
+
+# Refresh to get the access token.
+access_token = refresh_token.refresh!
+
+#
+# Get the owner information for the refresh token.
+#
+response = access_token.get('/oauth/verify')
+character_info = response.parsed
+character_id = character_info['CharacterID']
+
+#
+# Configure ESI with our access token.
+#
+ESI.configure do |conf|
+  conf.access_token = access_token.token
+end
+
+universe_api = ESI::UniverseApi.new
+corporation_api = ESI::CorporationApi.new
+character_api = ESI::CharacterApi.new
+
+#
+# From the public information about the character, locate the corporation ID.
+#
+character = character_api.get_characters_character_id(character_id)
+corporation_id = character.corporation_id
+
+#
+# Get the list of corporation structures.
+#
+structures = corporation_api.get_corporations_corporation_id_structures(corporation_id)
+
+# Remove any structures which aren't in the listed systems.
+system_names = universe_api.post_universe_ids(config[:systems]).systems
+system_ids = Set.new(system_names.map(&:id))
+structures.delete_if do |structure|
+  !system_ids.include?(structure.system_id)
+end
+
+# Sort by fuel expiry time.
+structures.sort_by!(&:fuel_expires)
+
+def left_to_state(left)
+  if left > 14
+    'good'
+  elsif left > 7
+    'warning'
+  else
+    'danger'
+  end
+end
+
+# Map from the raw structure to a set of useful properties
+structures.map! do |s|
+  # Public information for the structure.
+  pub = universe_api.get_universe_structures_structure_id(s.structure_id)
+  time_left = s.fuel_expires - DateTime.now
+  {
+    structure_id: s.structure_id,
+    system: pub.name.sub(/ - .*$/, ''),
+    name: pub.name.sub(/^.* - /, ''),
+    time: s.fuel_expires,
+    left: time_left,
+    state: left_to_state(time_left),
+    type_id: s.type_id
+  }
+end
+
+# Initialise state store.
+store = YAML::Store.new(config[:statefile])
+store.transaction do
+  store[:state] = {} unless store[:state]
+end
+
+# Remove any structures which are in the same state as last time
+structures.delete_if do |structure|
+  store.transaction do
+    structure[:old_state] = store[:state][structure[:structure_id]] || 'unknown'
+  end
+  structure[:state] == structure[:old_state]
+end
+
+# Map each remaining structure to a Slack attachment
+attachments = structures.map do |s|
+  eve_time = s[:time].strftime('%A, %Y-%m-%d %H:%M:%S EVE time')
+  {
+    title: s[:name] + ' in ' + s[:system],
+    color: s[:state],
+    text: "Fuel expires in #{format('%.1f', s[:left])} days.\n" \
+          "Services will go offline at #{eve_time}.\n" \
+          "Old state: #{s[:old_state]}, new state: #{s[:state]}",
+    fallback: "#{s[:name]} in #{s[:system]} fuel state is #{s[:state]}.",
+    thumb_url: "https://imageserver.eveonline.com/Render/#{s[:type_id]}_128.png"
+  }
+end
+
+# If we have something that is other than a 'good' state, take special action
+panic = structures.find_index { |s| s[:state] != 'good' }
+panic_text = panic ? '<!channel> :scream:' : ''
+
+#
+# Configure Slack.
+#
+
+slack_config = config[:slack]
+notifier = Slack::Notifier.new slack_config[:webhook_url] do
+  defaults slack_config[:defaults]
+end
+
+#
+# Send a Slack ping if we have anything to say.
+#
+unless attachments.empty?
+  notifier.ping panic_text + 'Structure fuel state changes:',
+                attachments: attachments
+end
+
+#
+# Write the state of these structures back for next time.
+#
+store.transaction do
+  structures.each do |s|
+    store[:state][s[:structure_id]] = s[:state]
+  end
+end
